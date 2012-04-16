ce68dc481826a3092f6383d42b6aaccabd115552
notiontaxi <=>
2012-04-16 12:22:46 +0200

    new files

---
+++ src/main/java/aiingames/samplesim/agents/MothAgent.java
@@ -0,0 +1,84 @@
+package aiingames.samplesim.agents;
+
+import aiingames.samplesim.Config;
+import aiingames.samplesim.simulation.Environment;
+import aiingames.samplesim.spatial.Object2D;
+import aiingames.samplesim.spatial.Vector2D;
+import aiingames.samplesim.spatial.Coordinate;
+
+import java.util.*;
+
+public class MothAgent implements Moveable {
+
+	
+	private final String id;
+	private double desiredVx;
+	private double desiredVy;
+	
+	
+	public MothAgent(String id) {
+		this.id = id;
+		this.desiredVx = -0.5;
+		this.desiredVy = -0.5;
+	}
+	
+	public void move(Environment e) {
+		
+//		a = (x, y)
+//		|a| = sqrt( x*x + y*y ) 
+		
+		Object2D my2Drep = e.getAgent(this.id);
+		
+		Vector2D newDir = new Vector2D(0.0, 0.0);
+		
+		for( Object2D light : e.getLights().values()){
+
+			Vector2D v1,v2;
+			v1 = (new Vector2D(light.getPosition().getX() - my2Drep.getPosition().getX(), my2Drep.getPosition().getY() - light.getPosition().getY())).normalize();
+			v2 = my2Drep.getDirection().normalize();
+			v2.setY(v2.getPosition().getY() * (-1));
+			
+			double angle = v1.dot(v2); 
+
+			if(angle <= 1.0 && angle >= 0.31){
+				double distance = light.getPosition().getDistanceTo(my2Drep.getPosition());
+				if(distance < 5){
+					v1 = v1.mult(12/distance);
+					newDir.plusEquals(v1);
+				}
+			}
+		}
+		newDir = newDir.normalize();
+		this.desiredVx = newDir.getX();
+		this.desiredVy = -newDir.getY();
+		
+		
+//		ArrayList<Coordinate> lights =  e.getLightCoordinates();
+//				
+//		for(Coordinate lc : lights)
+//			System.out.println(lc.getX());
+		
+		double vx = my2Drep.getDirection().getX();
+		double vy = my2Drep.getDirection().getY();
+		
+		if (vx == 0 && vy == 0) {
+			this.desiredVx = Config.MAX_V * 2 * (Math.random()-0.5);
+			this.desiredVy = Config.MAX_V * 2* (Math.random()-0.5);
+		}
+
+	}
+
+	public String getId() {
+		return this.id;
+	}
+
+	public double getDesiredVx() {
+		return this.desiredVx;
+	}
+
+	public double getDesiredVy() {
+		return this.desiredVy;
+	}
+
+
+}
---
+++ src/main/java/aiingames/samplesim/simulation/Environment.java
@@ -0,0 +1,170 @@
+package aiingames.samplesim.simulation;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import aiingames.samplesim.Config;
+import aiingames.samplesim.agents.Moveable;
+import aiingames.samplesim.agents.PointLight;
+import aiingames.samplesim.spatial.Coordinate;
+import aiingames.samplesim.spatial.Object2D;
+import aiingames.samplesim.spatial.Vector2D;
+
+public class Environment {
+	
+	private double minX = 0;
+	private double minY = 0;
+	private double maxX = 12;
+	private double maxY = 12;
+
+	Map<String, Object2D> physicalAgents = new HashMap<String, Object2D>();
+	
+	// --- ADDED		
+	Map<String, Object2D> physicalLights = new HashMap<String, Object2D>();
+	// --- END ADDED	
+	
+	protected void createAndAddPhysicalAgentRepresentation(Moveable agent,Coordinate c) {
+		Object2D pa = new Object2D(c);
+		pa.setDirection(new Vector2D(2.0, 2.0));
+		this.physicalAgents.put(agent.getId(), pa);
+		
+	}
+	
+// --- ADDED	
+	public void createAndAddPhysicalLightRepresentation(Moveable light, Coordinate c) {
+		Object2D pl = new Object2D(c);
+		this.physicalLights.put(light.getId(),pl);
+		
+	}	
+	
+
+
+
+	public void moveAgent(Moveable a) {
+		Object2D pa = this.physicalAgents.get(a.getId());
+//		pa.setDirection(new Vector2D(-0.5, 0.1));
+		
+		double dx = Config.getSimStepSize()*(pa.getDirection().getX() + a.getDesiredVx())/Config.TAU;
+		double dy = Config.getSimStepSize()*(pa.getDirection().getY() + a.getDesiredVy())/Config.TAU;
+		double nVx = pa.getDirection().getX() + dx;
+		double nVy = pa.getDirection().getY() + dy;
+		
+		double scale = validateV(nVx, nVy); // value between 0 and 1
+		
+		//double mvX = scale * nVx * Config.getSimStepSize();
+		//double mvY = scale * nVy * Config.getSimStepSize();
+		
+		double nx = (pa.getPosition().getX() + nVx*scale* Config.getSimStepSize() );
+		double ny = (pa.getPosition().getY() + nVy*scale* Config.getSimStepSize() );
+		
+		Coordinate nc = new Coordinate(nx, ny);
+		//System.out.println(pa.getDirection().getX() +"  "+pa.getPosition().getX());
+		Vector2D vec = new Vector2D(nVx,nVy).normalize();
+		
+		if (!checkCollision(pa.getPosition(),nc)) {
+			pa.setPosition(nc);
+			pa.setDirection(vec);
+		} else {
+			pa.setDirection(new Vector2D(0.0,0.0));
+		}
+		
+		
+	}
+
+
+	private double validateV(double nVx, double nVy) {
+
+		double v = Math.hypot(nVx, nVy);
+		if (v > Config.MAX_V) {
+			return Config.MAX_V / v;
+		}
+		return 1;
+	}
+
+
+	private boolean checkCollision(Coordinate c, Coordinate nc) {
+		if (nc.getX() < this.getMinX()) {
+			return true;
+		}
+		if (nc.getY() < this.getMinY()) {
+			return true;
+		}
+		if (nc.getX() > this.getMaxX()) {
+			return true;
+		}
+		if (nc.getY() > this.getMaxY()) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+
+	public double getMinX() {
+		return minX;
+	}
+
+
+	public void setMinX(double minX) {
+		this.minX = minX;
+	}
+
+
+	public double getMinY() {
+		return minY;
+	}
+
+
+	public void setMinY(double minY) {
+		this.minY = minY;
+	}
+
+
+	public double getMaxX() {
+		return maxX;
+	}
+
+
+	public void setMaxX(double maxX) {
+		this.maxX = maxX;
+	}
+
+
+	public double getMaxY() {
+		return maxY;
+	}
+
+
+	public void setMaxY(double maxY) {
+		this.maxY = maxY;
+	}
+
+
+
+	public Object2D getAgent(String _id){
+		
+		return this.physicalAgents.get(_id);
+	}
+
+
+
+	public Map<String, Object2D> getLights() {
+		return this.physicalLights;
+	}	
+	
+	public Map<String, Object2D> getAgents() {
+		return this.physicalAgents;
+	}
+
+	public void moveLight(Moveable light) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+
+}
